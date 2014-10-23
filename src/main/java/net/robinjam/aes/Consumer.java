bb5ab3fb793af1cf9bf45be44bc580957f1f0d9e
Peter Prince <pete.c.prince@gmail.com>
2014-10-23 12:37:12 +0100

    Add broker URL and topics as command line args

--- src/main/java/net/robinjam/aes/Consumer.java
+++ src/main/java/net/robinjam/aes/Consumer.java
@@ -16,11 +16,33 @@ import org.w3c.dom.Element;
 public class Consumer implements org.apache.cxf.wsn.client.Consumer.Callback {
 
 	public static void main(String[] args) throws Exception {
-		org.apache.cxf.wsn.client.Consumer consumer = new org.apache.cxf.wsn.client.Consumer(new Consumer(), "http://localhost:9001/TestConsumer");
-		NotificationBroker broker = new NotificationBroker("http://localhost:9000/wsn/NotificationBroker");
-		Subscription subscription = broker.subscribe(consumer, "LTFE");
-		Thread.sleep(1000 * 60);
-		subscription.unsubscribe();
+		String[] topics;
+		if (args.length < 2) {
+			System.err.println("Usage: mvn -Pconsumer package exec:java -Dexec.args \"[Broker Address] [Subscription Topic]+\"");
+			System.exit(1);
+		} else {
+			String address = args[0];
+			int topic_count = args.length - 1;
+			topics = new String[topic_count];
+			for (int i = 1; i < args.length; i++) {
+				topics[i-1] = args[i];
+			}
+			
+			org.apache.cxf.wsn.client.Consumer consumer = new org.apache.cxf.wsn.client.Consumer(new Consumer(), "http://localhost:9001/TestConsumer");
+			NotificationBroker broker = new NotificationBroker(address);
+			
+			Subscription[] subscriptions = new Subscription[topic_count];
+			for(int i=0; i<topic_count; i++){
+				subscriptions[i] = broker.subscribe(consumer, topics[i]);
+				System.out.println("Subscribed to " + topics[i]);
+			}
+			
+			Thread.sleep(1000 * 60);
+			for(int i=0; i<topic_count; i++){
+				subscriptions[i].unsubscribe();
+				System.out.println("Unsubscribed from " + topics[i]);
+			}
+		}
 	}
 
 	@Override
