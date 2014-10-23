d85dc43dcfb1ec2e3a3a5b5efd4784c0d2a90e0b
Peter Prince <pete.c.prince@gmail.com>
2014-10-23 13:32:05 +0100

    Add XML location as command line arg

--- src/main/java/net/robinjam/aes/Producer.java
+++ src/main/java/net/robinjam/aes/Producer.java
@@ -10,15 +10,16 @@ import org.w3c.dom.NodeList;
 public class Producer {
 
 	public static void main(String[] args) throws Exception {
-		if(args.length != 1) {
-			System.err.println("Usage: mvn -Pproducer package exec:java -Dexec.args \"[Broker Address]\"");
+		if(args.length != 2) {
+			System.err.println("Usage: mvn -Pproducer package exec:java -Dexec.args \"[Broker Address] [Delta Location]\"");
 			System.exit(1);
 		} else {
 			NotificationBroker broker = new NotificationBroker(args[0]);
 			
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			dbf.setNamespaceAware(true);
-			Document doc = dbf.newDocumentBuilder().parse(Producer.class.getResourceAsStream("/delta.xml"));
+			Document doc = dbf.newDocumentBuilder().parse(Producer.class.getResourceAsStream(args[1]));
+			System.out.println("Loaded XML delta from " + args[1]);
 			
 			NodeList events = doc.getElementsByTagNameNS("http://www.aixm.aero/schema/5.1/event", "Event");
 			for (int i = 0; i < events.getLength(); ++i) {
