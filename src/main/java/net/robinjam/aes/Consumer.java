249f72b527bf3a58596e62b5dde23657067d8323
James Robinson <admin@robinjam.net>
2014-10-24 12:34:51 +0100

    Allow bind address to be specified in consumer

--- src/main/java/net/robinjam/aes/Consumer.java
+++ src/main/java/net/robinjam/aes/Consumer.java
@@ -17,19 +17,20 @@ public class Consumer implements org.apache.cxf.wsn.client.Consumer.Callback {
 
 	public static void main(String[] args) throws Exception {
 		String[] topics;
-		if (args.length < 2) {
-			System.err.println("Usage: mvn -Pconsumer package exec:java -Dexec.args=\"[Broker Address] [Subscription Topic]+\"");
+		if (args.length < 3) {
+			System.err.println("Usage: mvn -Pconsumer package exec:java -Dexec.args=\"[Broker Address] [Bind Address] [Subscription Topic]+\"");
 			System.exit(1);
 		} else {
-			String address = args[0];
-			int topic_count = args.length - 1;
+			String brokerAddress = args[0];
+			String bindAddress = args[1];
+			int topic_count = args.length - 2;
 			topics = new String[topic_count];
-			for (int i = 1; i < args.length; i++) {
-				topics[i-1] = args[i];
+			for (int i = 2; i < args.length; i++) {
+				topics[i-2] = args[i];
 			}
 			
-			org.apache.cxf.wsn.client.Consumer consumer = new org.apache.cxf.wsn.client.Consumer(new Consumer(), "http://localhost:9001/TestConsumer");
-			NotificationBroker broker = new NotificationBroker(address);
+			org.apache.cxf.wsn.client.Consumer consumer = new org.apache.cxf.wsn.client.Consumer(new Consumer(), bindAddress);
+			NotificationBroker broker = new NotificationBroker(brokerAddress);
 			
 			Subscription[] subscriptions = new Subscription[topic_count];
 			for(int i=0; i<topic_count; i++){
