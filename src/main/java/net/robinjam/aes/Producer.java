94e182363e1a57fb637cffe83d1fd263f2f08a42
Charles Sherman <shermzor@gmail.com>
2014-10-27 11:24:16 +0000

    Make producer constructor

--- src/main/java/net/robinjam/aes/Producer.java
+++ src/main/java/net/robinjam/aes/Producer.java
@@ -1,16 +1,22 @@
 package net.robinjam.aes;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.cxf.wsn.client.NotificationBroker;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class Producer {
 
+	private NotificationBroker broker;
+	
 	public static void main(String[] args) throws Exception {
 		if(args.length != 2) {
 			System.err.println("Usage: mvn -Pproducer package exec:java -Dexec.args=\"[Broker Address] [Delta Location]\"");
@@ -30,6 +36,25 @@ public class Producer {
 				broker.notify(topic, event);
 				System.out.println("Sent event on topic " + topic);
 			}
+		}
+	}
+	
+	public Producer(String address) {
+		broker = new NotificationBroker(address);
+	}
+	
+	public void sendXML(String xmlFile) throws FileNotFoundException, SAXException, IOException, ParserConfigurationException {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		dbf.setNamespaceAware(true);
+		Document doc = dbf.newDocumentBuilder().parse(new FileInputStream(xmlFile));
+		System.out.println("Loaded XML delta from " + xmlFile);
+		
+		NodeList events = doc.getElementsByTagNameNS("http://www.aixm.aero/schema/5.1/event", "Event");
+		for (int i = 0; i < events.getLength(); ++i) {
+			Element event = (Element) events.item(i);
+			String topic = event.getElementsByTagNameNS("http://www.aixm.aero/schema/5.1/event", "location").item(0).getTextContent();
+			broker.notify(topic, event);
+			System.out.println("Sent event on topic " + topic);
 		}
 	}
 }
