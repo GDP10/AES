aba54d669e50119ef6b89c5499d7e93a38c27e08
James Robinson <admin@robinjam.net>
2014-11-11 12:02:48 +0000

    Refactor main method in Producer

--- src/main/java/net/robinjam/aes/Producer.java
+++ src/main/java/net/robinjam/aes/Producer.java
@@ -23,20 +23,8 @@ public class Producer {
 			System.err.println("Usage: mvn -Pproducer package exec:java -Dexec.args=\"[Broker Address] [Delta Location]\"");
 			System.exit(1);
 		} else {
-			NotificationBroker broker = new NotificationBroker(args[0]);
-			
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			dbf.setNamespaceAware(true);
-			Document doc = dbf.newDocumentBuilder().parse(new FileInputStream(args[1]));
-			System.out.println("Loaded XML delta from " + args[1]);
-			
-			NodeList events = doc.getElementsByTagNameNS("http://www.aixm.aero/schema/5.1/event", "Event");
-			for (int i = 0; i < events.getLength(); ++i) {
-				Element event = (Element) events.item(i);
-				String topic = event.getElementsByTagNameNS("http://www.aixm.aero/schema/5.1/event", "location").item(0).getTextContent();
-				broker.notify(topic, event);
-				System.out.println("Sent event on topic " + topic);
-			}
+			Producer producer = new Producer(args[0]);
+			producer.sendXML(new FileInputStream(args[1]));
 		}
 	}
 	
