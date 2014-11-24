7be88601ae69a28dbcc76b03611fcc80b271b9c6
Charles Sherman <shermzor@gmail.com>
2014-11-24 14:56:19 +0000

    Add temporal filtering cucumber tests

--- src/main/java/net/robinjam/aes/Consumer.java
+++ src/main/java/net/robinjam/aes/Consumer.java
@@ -1,5 +1,10 @@
 package net.robinjam.aes;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javax.jms.Connection;
 import javax.jms.Destination;
 import javax.jms.JMSException;
@@ -7,26 +12,76 @@ import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.security.auth.callback.Callback;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
-public class Consumer {
+public class Consumer implements Callback {
 	
 	public static void main(String[] args) throws JMSException {
-		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(args[0]);
+		Consumer consumer = new Consumer(args[0], args[1], args[2]);
+	}
+	
+	private Callback callback;
+	
+	public Consumer(Callback callback, String brokerAddress, String startDateString, String endDateString) throws JMSException {
+		
+		this.callback = callback;
+		this.startListening(brokerAddress, startDateString, endDateString);
+	}
+	
+	public Consumer(String brokerAddress, String startDateString, String endDateString) throws JMSException {
+		
+		this.callback = new SystemOutCallback();
+		this.startListening(brokerAddress, startDateString, endDateString);
+	}
+	
+	private void startListening(String brokerAddress, String startDateString, String endDateString) throws JMSException {
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerAddress);
 		Connection connection = connectionFactory.createConnection();
 		connection.start();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		Destination destination = session.createQueue("AES");
-		MessageConsumer consumer = session.createConsumer(destination, "startTime > 30");
+		
+		long startTimeSub, endTimeSub;
+		
+		try {
+			Date startDate = new SimpleDateFormat("yyyy-dd-MM'T'HH:mm:ss", Locale.ENGLISH).parse(startDateString);
+			Date endDate = new SimpleDateFormat("yyyy-dd-MM'T'HH:mm:ss", Locale.ENGLISH).parse(endDateString);
+			// time is given in milliseconds, divide by 1000 to get a unix timestamp in seconds
+			startTimeSub = startDate.getTime() / 1000;
+			endTimeSub = endDate.getTime() / 1000;
+		}
+		catch(ParseException e) {
+			startTimeSub = -1;
+			endTimeSub = -1;
+		}
+		
+		MessageConsumer consumer = session.createConsumer(destination, "startTime < " + endTimeSub + " AND endTime > " + startTimeSub);
+		
 		Message message = consumer.receive(60000);
 		
 		if (message instanceof TextMessage) {
-			System.out.println(((TextMessage) message).getText());
+			callback.recieveMessage((TextMessage) message);
 		}
 		
 		consumer.close();
 		session.close();
 		connection.close();
+	}
+	
+	public static interface Callback {
+		
+		public void recieveMessage(TextMessage message) throws JMSException;
+		
+	}
+	
+	private static class SystemOutCallback implements Callback {
+
+		@Override
+		public void recieveMessage(TextMessage message) throws JMSException {
+			System.out.println(message.getText());
+		}
+		
 	}
 }
--- src/main/java/net/robinjam/aes/Producer.java
+++ src/main/java/net/robinjam/aes/Producer.java
@@ -1,7 +1,7 @@
 package net.robinjam.aes;
 
-	import java.io.File;
-import java.io.FileInputStream;
+	import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
@@ -34,8 +34,12 @@ import org.xml.sax.SAXException;
 public class Producer {
 	
 	public static void main(String[] args) throws JMSException, SAXException, IOException, ParserConfigurationException, TransformerException {
+		Producer producer = new Producer(args[0], Producer.class.getResourceAsStream(args[1]));
+	}
+	
+	public Producer(String brokerAddress, InputStream inputStreamMaster) throws JMSException, SAXException, IOException, ParserConfigurationException, TransformerException {
 		
-		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(args[0]);
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerAddress);
 		Connection connection = connectionFactory.createConnection();
 		connection.start();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
@@ -43,32 +47,29 @@ public class Producer {
 		MessageProducer producer = session.createProducer(destination);
 		producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 		
-		InputStream inputStream = new FileInputStream(new File(args[1]));
-		List<Element> notams = XMLParser.parseNOTAMS(inputStream); 
+		// Allows us to re-use the input stream for multiple parses
+		CopyInputStream inputStream = new CopyInputStream(inputStreamMaster);
+		
+		List<Element> notams = XMLParser.parseNOTAMS(inputStream.getCopy()); 
 		
 		for(int i = 0; i < notams.size(); i++) {
 			
 			// Send the string extracted from the element
 			TextMessage message = session.createTextMessage(getElementAsString(notams.get(i)));
 	
-			// Re-make the stream so we can parse again
-			inputStream = new FileInputStream(new File(args[1]));
-
 			long startTime, endTime;
 			
 			try {
-				Date startDate = new SimpleDateFormat("yyyy-dd-MM'T'HH:mm:ss", Locale.ENGLISH).parse(XMLParser.parseElement(inputStream, "http://www.opengis.net/gml/3.2", "beginPosition").get(0).getTextContent());
+				Date startDate = new SimpleDateFormat("yyyy-dd-MM'T'HH:mm:ss", Locale.ENGLISH).parse(XMLParser.parseElement(inputStream.getCopy(), "http://www.opengis.net/gml/3.2", "beginPosition").get(0).getTextContent());
 				// time is given in milliseconds, divide by 1000 to get a unix timestamp in seconds
-				startTime = startDate.getTime();
+				startTime = startDate.getTime() / 1000;
 			}
 			catch(ParseException e) {
 				startTime = -1;
 			}
 			
-			inputStream = new FileInputStream(new File(args[1]));
-			
 			try {
-				Date endDate = new SimpleDateFormat("yyyy-dd-MM'T'HH:mm:ss", Locale.ENGLISH).parse(XMLParser.parseElement(inputStream, "http://www.opengis.net/gml/3.2", "endPosition").get(0).getTextContent());
+				Date endDate = new SimpleDateFormat("yyyy-dd-MM'T'HH:mm:ss", Locale.ENGLISH).parse(XMLParser.parseElement(inputStream.getCopy(), "http://www.opengis.net/gml/3.2", "endPosition").get(0).getTextContent());
 				// time is given in milliseconds, divide by 1000 to get a unix timestamp in seconds
 				endTime = endDate.getTime() / 1000;
 			}
@@ -88,7 +89,7 @@ public class Producer {
 		
 		session.close();
 		connection.close();
-		
+	
 	}
 	
 	private static String getElementAsString(Element element) throws TransformerException {
@@ -102,5 +103,37 @@ public class Producer {
 		String elementString = buffer.toString();
 		
 		return elementString;
+	}
+	
+	private static class CopyInputStream {
+	    private InputStream _is;
+	    private ByteArrayOutputStream _copy = new ByteArrayOutputStream();
+
+	    public CopyInputStream(InputStream is) {
+	        _is = is;
+
+	        try {
+	            copy();
+	        }
+	        catch(IOException ex) {
+	        }
+	    }
+
+	    private int copy() throws IOException {
+	        int read = 0;
+	        int chunk = 0;
+	        byte[] data = new byte[256];
+
+	        while(-1 != (chunk = _is.read(data))) {
+	            read += data.length;
+	            _copy.write(data, 0, chunk);
+	        }
+
+	        return read;
+	    }
+
+	    public InputStream getCopy() {
+	        return (InputStream)new ByteArrayInputStream(_copy.toByteArray());
+	    }
 	}
 }
